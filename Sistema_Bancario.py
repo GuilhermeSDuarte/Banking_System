3cd672801bdedbd19e50a60429cd624c05bf177e
Guilherme Duarte <57077716+GuilhermeSDuarte@users.noreply.github.com>
2023-07-04 22:21:00 -0300

    Aprimorando o código
    Nesta atualização, foquei em corrigir futuros problemas e tratei erros de exceção como digitar um char em um input que é convertido em inteiro.

--- Sistema_Bancario.py
+++ Sistema_Bancario.py
@@ -21,43 +21,80 @@ Bem vindo ao CashBank
     [0]- Sair
     
 """.format(saldo=saldo))
+
     selecionar = input("Digite a Opção:")
 
     if selecionar == "1":
+
         print("\nDepósito")
-        valor = float(input("\nDigite o valor que deseja depositar: "))
-        if valor <= 0:
-            saldo += valor
-            extrato += f"Depósito: R$ {valor:.2f}\n"
-        else:
-            print("Valor invalido")
-        selecionar = input("\ntoque em qualquer tecla para continuar com operações ou 0 para sair: ")
-        if selecionar == "0":
-            break
-    elif selecionar == "2" and qtd_saques < LIMITE_SAQUES:
-        print("\nSaque")
-        valor = float(input("\nDigite o valor que deseja sacar: "))
-        if valor <= 500.0:
-            if saldo > valor:
-                saldo -= valor
-                extrato += f"Saque: R$ {valor:.2f}\n"
+
+        try:
+
+            valor = int(input("\nDigite o valor que deseja depositar: "))
+
+            if valor <= 0:
+                print("Valor invalido")
+
             else:
-                print("Você não possui saldo suficiente.")
-        else:
-            print("\nO Limite Maximo por saque é R$500,00.")
-        qtd_saques += 1
-        selecionar = input("\nToque em qualquer tecla para continuar com operações ou 0 para sair: ")
-        if selecionar == "0":
-            break
+                saldo += valor
+                extrato += f"Depósito: R$ {valor:.2f}\n"
+
+            selecionar = input("\ntoque em qualquer tecla para continuar com operações ou 0 para sair: ")
+
+            if selecionar == "0":
+                break
+
+        except ValueError:
+            print("O valor digitado não é numérico, tente novamente.")
+
+    elif selecionar == "2" and qtd_saques < LIMITE_SAQUES:
+
+        print("\nSaque")
+
+        try:
+
+            valor = int(input("\nDigite o valor que deseja sacar: "))
+
+            if valor <= 500:
+
+                if saldo > valor:
+                    saldo -= valor
+                    extrato += f"Saque: R$ {valor:.2f}\n"
+                else:
+                    print("Você não possui saldo suficiente.")
+
+            else:
+                print("\nO Limite Maximo por saque é R$500,00.")
+
+            qtd_saques += 1
+
+            selecionar = input("\nToque em qualquer tecla para continuar com operações ou 0 para sair: ")
+
+            if selecionar == "0":
+                break
+
+        except ValueError:
+            print("O valor digitado não é numérico, tente novamente.")
+
+    elif selecionar == "2" and qtd_saques == LIMITE_SAQUES:
+
+        print("Limite de Saques excedidos.")
+        print(f"Saques:{qtd_saques}".format(qtd_saques=qtd_saques))
+
     elif selecionar == "3":
+
         print("\n================== Extrato ====================")
         print("Não foram realizadas movimentações." if not extrato else extrato)
         print(f"\nSaldo: R$ {saldo:.2f}")
+        print("\n===============================================")
 
         selecionar = input("toquem em qualquer tecla para continuar com operações ou 0 para sair: ")
+
         if selecionar == "0":
             break
+
     elif selecionar == "0":
         break
+
     else:
         print("Opção Invalida, digite uma opção valida.")
